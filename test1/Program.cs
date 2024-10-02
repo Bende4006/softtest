8354a685a7bff29506e76952a817fa0c3bb549e7
Bende4006 <szkybend@gmail.com>
2024-10-02 10:43:40 +0200

    test

---
+++ test1/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] szinek = { "piros", "kék", "sárga", "zöld", "fekete" };
+            int i;
+            for (i=0; i<szinek.Length;i++)
+            {
+                Console.WriteLine(szinek[i]);
+            }
+            Console.WriteLine("Adj meg egy szint ");
+            string szin;
+            szin = Console.ReadLine();
+
+            for (i = 0; i < szinek.Length; i++)
+            {
+                if (szin==szinek[i])
+                {
+                    Console.WriteLine("van ilyen szin");
+                }
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
