2df541e62ca2ed8ef88975359cf434635137df67
Bende4006 <szkybend@gmail.com>
2024-12-04 11:52:54 +0100

    doga

---
+++ doga12.04/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace doga12._04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("Add meg az évszámot: ");
+            int szsz = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add meg a halál évszámát: ");
+            int hasz = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Add meg a nevet: ");
+            string nev = Convert.ToString(Console.ReadLine());
+
+            int idos = hasz - szsz;
+            Console.WriteLine($"Enyi idősen hallt meg {nev}: {idos} ");
+
+            Console.ReadKey();
+
+      
+
+
+        }
+    }
+}
